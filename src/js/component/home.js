7b3d9e9b28108daa9c0e432ffa10eb3d2869efeb
legg2710 <luisemilio2710@gmail.com>
2021-06-23 01:40:35 +0000

    To Do List Preview

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,47 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState } from "react";
 export function Home() {
+	// 01 - Crear un array
+	const [todos, setTodos] = useState(["tarea 1", "tareas 2", "tarea 3"]);
+	/// 02 guardar texto que digito el usuario
+	const [task, setTask] = useState("");
 	return (
+		// 03 Hacer el formulario
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>TODO LIST</h1>
+			<span>{JSON.stringify(task)}</span>
+			<form
+				onSubmit={evento => {
+					evento.preventDefault();
+					setTodos([...todos, task]);
+					setTask("");
+				}}>
+				<input
+					placeholder="Ingrese su tarea"
+					onChange={evento => setTask(evento.target.value)}
+					value={task}></input>
+				<button>Agregar</button>
+			</form>
+			{/* 04 mostrar el contenido del arreglo */}
+			<ul>
+				{todos.map((item, index) => {
+					return (
+						<li key={index}>
+							<span>{item}</span>
+							<button
+								onClick={() =>
+									console.log(
+										"eliminar elemento",
+										index,
+										"contenido:",
+										todos[index]
+									)
+								}>
+								X
+							</button>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
