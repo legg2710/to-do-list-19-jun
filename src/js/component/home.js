1680b66bad2d5eaf94024b704d08a0039395a40d
legg2710 <luisemilio2710@gmail.com>
2021-06-25 01:13:02 +0000

    todolist 95%

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,47 +1,68 @@
 import React, { useState } from "react";
 export function Home() {
-	// 01 - Crear un array
-	const [todos, setTodos] = useState(["tarea 1", "tareas 2", "tarea 3"]);
-	/// 02 guardar texto que digito el usuario
+	const [todos, setTodos] = useState([]);
 	const [task, setTask] = useState("");
+	function todosEliminar(index) {
+		if (index > -1) {
+			console.log("eliminar");
+			const filterList = todos.filter(item => item !== todos[index]);
+			setTodos(filterList);
+		}
+	}
 	return (
-		// 03 Hacer el formulario
 		<div className="text-center mt-5">
-			<h1>TODO LIST</h1>
-			<span>{JSON.stringify(task)}</span>
+			<h1 className="display-4">To Do List</h1>
+
 			<form
 				onSubmit={evento => {
 					evento.preventDefault();
 					setTodos([...todos, task]);
 					setTask("");
 				}}>
-				<input
-					placeholder="Ingrese su tarea"
-					onChange={evento => setTask(evento.target.value)}
-					value={task}></input>
-				<button>Agregar</button>
+				<div className="input-group mb-3">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Ingrese su tarea"
+						onChange={evento => setTask(evento.target.value)}
+						value={task}></input>
+					<div className="input-group-append">
+						<button
+							className="btn btn-outline-secondary"
+							type="button">
+							Agregar
+						</button>
+					</div>
+				</div>
 			</form>
 			{/* 04 mostrar el contenido del arreglo */}
-			<ul>
+			<ul className="list-group list-group-flush">
 				{todos.map((item, index) => {
 					return (
-						<li key={index}>
+						<li className="list-group-item" key={index}>
 							<span>{item}</span>
 							<button
-								onClick={() =>
-									console.log(
-										"eliminar elemento",
-										index,
-										"contenido:",
-										todos[index]
-									)
+								className="btn btn-light float-right"
+								onClick={
+									() => todosEliminar(index)
+									// index,
+									// "contenido:",
+									// todos[index]
 								}>
-								X
+								<i
+									className="fa fa-trash"
+									aria-hidden="true"></i>
 							</button>
 						</li>
 					);
 				})}
 			</ul>
+			<div>
+				<p className="text-muted">
+					{todos.length}
+					{""} item left
+				</p>
+			</div>
 		</div>
 	);
 }
